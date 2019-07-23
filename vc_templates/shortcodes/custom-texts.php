9f033de757649d6176fcb3997dd6fe0f47322e70
Syed Naqi Ali Shah <naqishah2997@gmail.com>
2019-07-23 03:20:21 -0700

    Changed Style for entry content

--- vc_templates/shortcodes/custom-texts.php
+++ vc_templates/shortcodes/custom-texts.php
@@ -1,5 +1,5 @@
 <?php
-
+// Money Advisor Title
 add_shortcode( 'sec_title', 'sec_title_func' );
 function sec_title_func( $atts ) {
  extract( shortcode_atts( array(
@@ -32,10 +32,57 @@ function section_title() {
   "class" => "",
   "heading" => __( "Text color", "my-text-domain" ),
   "param_name" => "color",
-  "value" => '#FF0000', //Default Red color
+  "value" => '#333e5e', //Default  color
   "description" => __( "Choose text color", "my-text-domain" )
  )
   )
  ) );
 }
+
+// End Money Advisor Title
+
+
+
+
+// Money Advisor Section Text 
+add_shortcode( 'sec_text', 'sec_text_func' );
+function sec_text_func( $atts ) {
+ extract( shortcode_atts( array(
+  'text' => 'Text Here',
+  'color' => '#666666'
+ ), $atts ) );
+  
+ return "<p class='section-text' style='color:{$color};'>{$text}</p>";
+}
+
+add_action( 'vc_before_init', 'section_text' );
+function section_text() {
+ vc_map( array(
+  "name" => __( "Money Advisor Text", "moneyadvisor_theme" ),
+  "base" => "sec_text",
+  "class" => "",
+  "category" => __( "Money Advisor", "moneyadvisor_theme"),
+  "params" => array(
+ array(
+  "type" => "textfield",
+  "holder" => "div",
+  "class" => "",
+  "heading" => __( "Section Text", "moneyadvisor_theme" ),
+  "param_name" => "title",
+  "value" => __( "Default param value", "moneyadvisor_theme" ),
+  "description" => __( "Description for Text param.", "moneyadvisor_theme" )
+ ),
+ array(
+  "type" => "colorpicker",
+  "class" => "",
+  "heading" => __( "Text color", "moneyadvisor_theme" ),
+  "param_name" => "color",
+  "value" => '#666666', //Default  color
+  "description" => __( "Choose text color", "moneyadvisor_theme" )
+ )
+  )
+ ) );
+}
+
+// End Money Advisor Section Text
 ?>
