c9d470a3b450b25a82432fd5987ac03969f98c3f
Syed Naqi Ali Shah <naqishah2997@gmail.com>
2019-07-23 17:23:38 -0700

    css changes

--- js/main.js
+++ js/main.js
@@ -125,5 +125,77 @@ $('.other-debts').owlCarousel({
         }
     }
 })
+
+
+
+
+/*=================== Sticky Menu ===================*/
+if(jQuery('header.header').length > 0){
+    var myNavBar = {
+
+        flagAdd: true,
+
+        elements: [],
+
+        init: function(elements) {
+            this.elements = elements;
+        },
+
+        add: function() {
+            if (this.flagAdd) {
+                for (var i = 0; i < this.elements.length; i++) {
+                    document.getElementById(this.elements[i]).className += " sticky-nav";
+                }
+                this.flagAdd = false;
+            }
+        },
+
+        remove: function() {
+            for (var i = 0; i < this.elements.length; i++) {
+                document.getElementById(this.elements[i]).className =
+                    document.getElementById(this.elements[i]).className.replace(/(?:^|\s)sticky-nav(?!\S)/g, '');
+            }
+            this.flagAdd = true;
+        }
+
+    };
+    /**
+     * Init the object. Pass the object the array of elements
+     * that we want to change when the scroll goes down
+     */
+    myNavBar.init([
+        "main-nav"
+    ]);
+
+    /**
+     * Function that manage the direction
+     * of the scroll
+     */
+    function offSetManager() {
+
+        var yOffset = 0;
+        var currYOffSet = window.pageYOffset;
+
+        if (yOffset < currYOffSet) {
+            myNavBar.add();
+        } else if (currYOffSet == yOffset) {
+            myNavBar.remove();
+        }
+    }
+    /**
+     * bind to the document scroll detection
+     */
+    window.onscroll = function(e) {
+        offSetManager();
+    }
+
+    /**
+     * We have to do a first detectation of offset because the page
+     * could be load with scroll down set.
+     */
+    offSetManager();
+  }
+
+
     
     })(jQuery);
